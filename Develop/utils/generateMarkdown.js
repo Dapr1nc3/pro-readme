5d6257c3c1d2e1ea70ea5427dc1366cd67a0c143
dapr1nc3 <ddaprincem84@gmail.com>
2021-12-15 21:51:58 -0500

    trying to get badges to work

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,131 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const fs = require('fs');
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderBadge(license, github, project) {
+  if (license){
+    return `https://img.shields.io/github/license/${github}/${project}`
+  };
+};
+
+function renderLink(license) {
+  if (license) {
+    console.log(license);
+    return `[${license} license](https://choosealicense.com/licenses/${license.toLowerCase()}/)`
+  }
+};
+
+function renderSection(license) {
+  if (license) {
+    return `## License🐱‍🏍`
+  };
+};
+ 
+
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function loadReadme(userInput) {
+  console.log(userInput);
+  return `
+  # ${userInput.project}
 
-`;
+  ## Description📝
+  ${userInput.description}
+
+  ## Table of Contents
+  * [Installation](#installation💽)
+  * [Usage](#usage🐱‍💻)
+  * [Credits](#credits👻)
+  * [License](#license🐱‍🏍)
+  * [Badges](#badges🎭)
+  * [Contributing](#contributing✔️)
+  * [Tests](#tests.🧪)
+  * [Questions](#questions❓)
+  
+
+  ## Description
+
+  ${userInput.description}
+
+
+  ## Installation💽
+
+  ${userInput.install}
+
+
+  ## Usage🐱‍💻
+
+  ${userInput.runtest}
+  ${userInput.repo}
+
+
+  ## Credits👻
+
+${userInput.contributingrepo}
+
+
+  ${renderSection(userInput.badges[0])}
+
+  ${renderLink(userInput.badges[0])}
+
+
+  ## Badges🎭
+
+  ${renderBadge(userInput.badges[0], userInput.name, userInput.project)}
+
+
+  ## Contributing✔️
+
+
+
+
+  ## Tests🧪
+
+
+
+
+  ## Questions❓
+
+${userInput.name}
+${userInput.email}
+
+[${userInput.name}](https://github.com/${userInput.name})
+
+  `;
 }
 
-module.exports = generateMarkdown;
+
+
+module.exports = loadReadme;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ dist/pro-readme.md
@@ -0,0 +1,66 @@
+
+  # pro-readme
+
+  ## Description📝
+  dfdfd
+
+  ## Table of Contents
+  * [Installation](#installation💽)
+  * [Usage](#usage🐱‍💻)
+  * [Credits](#credits👻)
+  * [License](#license🐱‍🏍)
+  * [Badges](#badges🎭)
+  * [Contributing](#contributing✔️)
+  * [Tests](#tests.🧪)
+  * [Questions](#questions❓)
+  
+
+  ## Description
+
+  dfdfd
+
+
+  ## Installation💽
+
+  dfdfdfd
+
+
+  ## Usage🐱‍💻
+
+  dfdfdfd
+  dfdfdf
+
+
+  ## Credits👻
+
+dfdfdf
+
+
+  ## License🐱‍🏍
+
+  [GNU license](https://choosealicense.com/licenses/gnu/)
+
+
+  ## Badges🎭
+
+  https://img.shields.io/github/license/Dapr1nc3/pro-readme
+
+
+  ## Contributing✔️
+
+
+
+
+  ## Tests🧪
+
+
+
+
+  ## Questions❓
+
+Dapr1nc3
+dfdfdfdf
+
+[Dapr1nc3](https://github.com/Dapr1nc3)
+
+  
